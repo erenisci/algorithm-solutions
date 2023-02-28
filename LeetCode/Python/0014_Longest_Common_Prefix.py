3ba7bc3d304a8d6df5aa45da191e51b182bcb7b0
Eren İşci <isci.eren@gmail.com>
2023-02-28 10:08:16 +0300

    P-0014

---
+++ LeetCode/Python/0014_Longest_Common_Prefix.py
@@ -0,0 +1,11 @@
+# 14. Longest Common Prefix
+# https://leetcode.com/problems/longest-common-prefix/description/
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+        ans = strs[0]
+        for i in strs:
+            while not i.startswith(ans):
+                ans = ans[:-1]
+        return ans
