61349025cc52b8947f88eb63bcee41223e80992c
Eren İşci <isci.eren@gmail.com>
2023-06-30 03:39:49 +0300

    P-0045

---
+++ LeetCode/Python/0045_Jump_Game_II.py
@@ -0,0 +1,16 @@
+# 45. Jump Game II
+# https://leetcode.com/problems/jump-game-ii/description/
+
+
+class Solution:
+    def jump(self, nums: list[int]) -> int:
+        answer = 0
+        current = next = 0
+        while next < len(nums) - 1:
+            distance = 0
+            for i in range(current, next + 1):
+                distance = max(distance, nums[i] + i)
+            current = next + 1
+            next = distance
+            answer += 1
+        return answer
