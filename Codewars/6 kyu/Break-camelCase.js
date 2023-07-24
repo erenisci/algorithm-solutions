86a51361892b9338947010ed401b5b3dd3ff7c77
Eren İşci <isci.eren@gmail.com>
2023-07-24 14:54:16 +0300

    Break camelCase

--- Codewars/6 kyu/Break-camelCase.js
+++
@@ -1,9 +0,0 @@
-function solution(string) {
-  let str = '';
-  for (let i = 0; i < string.length; i++) {
-    if (/[A-Z]/.test(string[i])) {
-      str += ' ' + string[i];
-    } else str += string[i];
-  }
-  return str;
-}
