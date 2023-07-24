ef1386e40436ca701dfff21d0b9aa5815b98ba61
Eren İşci <isci.eren@gmail.com>
2023-07-24 14:54:58 +0300

    A Needle in the Haystack

--- Codewars/8 kyu/A-Needle-in-the-Haystack.js
+++
@@ -1,4 +0,0 @@
-function findNeedle(haystack) {
-  if (haystack.includes('needle')) return 'found the needle at position ' + haystack.indexOf('needle');
-  else return "Your function didn't return anything";
-}
