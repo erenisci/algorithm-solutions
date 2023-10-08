330f07c3ef9edc139529abe150959dfe7a1a8a23
Eren İşci <isci.eren@gmail.com>
2023-10-09 01:08:20 +0300

    P-0899

---
+++ LeetCode/Python/0899_Orderly_Queue.py
@@ -0,0 +1,13 @@
+# 899. Orderly Queue
+# https://leetcode.com/problems/orderly-queue/submissions/
+
+
+class Solution:
+    def orderlyQueue(self, s: str, k: int) -> str:
+        if k > 1:
+            return "".join(sorted(s))
+        else:
+            ans = s
+            for i in range(1, len(s) + 1):
+                ans = min(ans, s[i : len(s)] + s[0:i])
+            return ans
