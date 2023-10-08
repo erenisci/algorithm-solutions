e317047c80c08cc91145dab8e5d776e46b8d7f0f
Eren İşci <isci.eren@gmail.com>
2023-10-09 01:08:32 +0300

    P-0904

---
+++ LeetCode/Python/0904_Fruit_Into_Baskets.py
@@ -0,0 +1,21 @@
+# 904. Fruit Into Baskets
+# https://leetcode.com/problems/fruit-into-baskets/description/
+
+
+class Solution:
+    def totalFruit(self, fruits: list[int]) -> int:
+        n = len(fruits)
+        i = j = m = 0
+        x, y = fruits[0], -1
+        while j < n:
+            if fruits[j] != x and y == -1:
+                y = fruits[j]
+            elif fruits[j] != x and fruits[j] != y:
+                x, y = fruits[j - 1], fruits[j]
+                m = max(m, j - i)
+                i = j - 1
+                while fruits[i - 1] == x:
+                    i -= 1
+            j += 1
+        m = max(m, j - i)
+        return m
