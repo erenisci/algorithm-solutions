d147abaa676cf29261e8dbdc67fd2e669831817f
Eren İşci <isci.eren@gmail.com>
2024-02-22 18:13:34 +0300

    Number of People in the Bus

--- Codewars/JavaScript/7 kyu/Number-of-People-in-the-Bus.js
+++ Codewars/JavaScript/7 kyu/Number-of-People-in-the-Bus.js
@@ -1,8 +1,8 @@
-var number = function (busStops) {
+function number(busStops) {
   let res = 0;
   for (let k = 0; k < busStops.length; k++) {
     res += busStops[k][0];
     res -= busStops[k][1];
   }
   return res;
-};
+}
