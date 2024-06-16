499009401023fdc15c627e821a552b7e56a067a5
Eren İşci <isci.eren@gmail.com>
2024-06-17 02:32:33 +0300

    del

--- Project-Euler/Python/p_001.py
+++
@@ -1,6 +0,0 @@
-# Multiples of 3 or 5, Problem #1
-# https://projecteuler.net/problem=1
-
-
-list = [i for i in range(1000) if i % 3 == 0 or i % 5 == 0]
-print("Sum:", sum(list))
--- Project-Euler/Python/p_015.py
+++
@@ -1,8 +0,0 @@
-# Lattice paths, Problem #15
-# https://projecteuler.net/problem=15
-
-
-import math
-
-steps = math.factorial(40) / (math.factorial(20) * math.factorial(20))
-print("Steps:", int(steps))
