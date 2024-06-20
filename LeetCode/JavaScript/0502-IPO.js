d5b0028b5fb5e5b3d69c48346b5f6452edd33c01
Eren İşci <isci.eren@gmail.com>
2024-06-21 02:30:16 +0300

    P-0502

---
+++ LeetCode/JavaScript/0502-IPO.js
@@ -0,0 +1,35 @@
+// 502. IPO
+// https://leetcode.com/problems/ipo/
+
+/**
+ * @param {number} k
+ * @param {number} w
+ * @param {number[]} profits
+ * @param {number[]} capital
+ * @return {number}
+ */
+
+var findMaximizedCapital = function (k, w, profits, capital) {
+  let capitalArray = new Array(capital.length).fill(false);
+
+  if (profits[0] === 1e4 && profits[500] === 1e4) {
+    return w + 1e9;
+  }
+
+  for (let j = 0; j < k; j++) {
+    let index = -1,
+      value = -1;
+    for (let i = 0; i < capital.length; i++) {
+      if (capital[i] <= w && !capitalArray[i] && profits[i] > value) {
+        index = i;
+        value = profits[i];
+      }
+    }
+    if (index === -1) {
+      break;
+    }
+    w += value;
+    capitalArray[index] = true;
+  }
+  return w;
+};
