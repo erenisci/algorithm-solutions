2dc446ed84bd27b39105098ad7c3ecbef510ee17
Eren İşci <isci.eren@gmail.com>
2024-06-23 19:06:58 +0300

    P-2235

---
+++ LeetCode/JavaScript/2235-Add-Two-Integers.js
@@ -0,0 +1,11 @@
+// 2235. Add Two Integers
+// https://leetcode.com/problems/add-two-integers/
+
+/**
+ * @param {number} num1
+ * @param {number} num2
+ * @return {number}
+ */
+var sum = function (num1, num2) {
+  return num1 + num2;
+};
