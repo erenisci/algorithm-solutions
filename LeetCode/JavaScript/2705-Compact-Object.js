0078169d9c1b80a69a6e375c737f0ecdbb2ff64e
Eren İşci <isci.eren@gmail.com>
2024-06-24 01:42:30 +0300

    P-2705

---
+++ LeetCode/JavaScript/2705-Compact-Object.js
@@ -0,0 +1,21 @@
+// 2705. Compact Object
+// https://leetcode.com/problems/compact-object/
+
+/**
+ * @param {Object|Array} obj
+ * @return {Object|Array}
+ */
+
+var compactObject = function (obj) {
+  if (obj === null) return null;
+  if (Array.isArray(obj)) return obj.filter(Boolean).map(compactObject);
+  if (typeof obj !== 'object') return obj;
+
+  const compacted = {};
+  for (const key in obj) {
+    let value = compactObject(obj[key]);
+    if (value) compacted[key] = value;
+  }
+
+  return compacted;
+};
