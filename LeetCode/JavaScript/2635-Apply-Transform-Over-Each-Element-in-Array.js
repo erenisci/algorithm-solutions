ac65bec0c8e340c0cb557a2037bc16691ad29ab2
Eren İşci <isci.eren@gmail.com>
2024-06-24 01:41:27 +0300

    P-2635

---
+++ LeetCode/JavaScript/2635-Apply-Transform-Over-Each-Element-in-Array.js
@@ -0,0 +1,15 @@
+// 2635. Apply Transform Over Each Element in Array
+// https://leetcode.com/problems/apply-transform-over-each-element-in-array/
+
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+
+var map = function (arr, fn) {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = fn(arr[i], i);
+  }
+  return arr;
+};
