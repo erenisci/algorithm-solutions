d8739210557a3ed92af61beec8af8ca35fdd45c3
Eren İşci <isci.eren@gmail.com>
2024-06-25 01:49:14 +0300

    P-0005

--- LeetCode/Python/0005_Longest_Palindromic_Substring.py
+++ LeetCode/Python/0005_Longest_Palindromic_Substring.py
@@ -1,5 +1,5 @@
 # 5. Longest Palindromic Substring
-# https://leetcode.com/problems/longest-palindromic-substring/description/
+# https://leetcode.com/problems/longest-palindromic-substring/
 
 
 class Solution:
