8a35dd190de810fac19ec59458abda6d47ed7062
Eren İşci <isci.eren@gmail.com>
2024-06-25 01:49:25 +0300

    P-0007

--- LeetCode/Python/0007_Reverse_Integer.py
+++ LeetCode/Python/0007_Reverse_Integer.py
@@ -2,17 +2,14 @@
 # https://leetcode.com/problems/reverse-integer/
 
 
-from math import pow
-
-
 class Solution:
     def reverse(self, x: int) -> int:
         if x < 0:
             x = int(str(x * -1)[::-1]) * -1
-            if x < pow(-2, 31):
+            if x < -(2**31):
                 return 0
         else:
             x = int(str(x)[::-1])
-            if x > pow(2, 31) - 1:
+            if x > 2**31 - 1:
                 return 0
         return x
