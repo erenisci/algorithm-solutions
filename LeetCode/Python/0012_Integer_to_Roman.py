6daf0133497bc587ed4c5532ec5d8b9c774477f8
Eren İşci <isci.eren@gmail.com>
2024-06-25 01:49:32 +0300

    P-0012

---
+++ LeetCode/Python/0012_Integer_to_Roman.py
@@ -0,0 +1,29 @@
+# 12. Integer to Roman
+# https://leetcode.com/problems/integer-to-roman/
+
+
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        numerals = [
+            "M",
+            "CM",
+            "D",
+            "CD",
+            "C",
+            "XC",
+            "L",
+            "XL",
+            "X",
+            "IX",
+            "V",
+            "IV",
+            "I",
+        ]
+
+        res, i = "", 0
+        while num:
+            res += (num // values[i]) * numerals[i]
+            num %= values[i]
+            i += 1
+        return res
