224863f49a4767bdf682bb9f9159759e1c13bdaf
Eren İşci <isci.eren@gmail.com>
2024-06-27 01:47:14 +0300

    P-0058

---
+++ LeetCode/Python/0058_Length_of_Last_Word.py
@@ -0,0 +1,9 @@
+# 58. Length of Last Word
+# https://leetcode.com/problems/length-of-last-word/
+
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        s = s.strip()
+        s = s.split(" ")
+        return len(s[-1])
