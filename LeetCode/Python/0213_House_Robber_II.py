3ec0981d1b4dd7b9fa91a3070e375345e4616abe
Eren İşci <isci.eren@gmail.com>
2024-06-30 10:37:03 +0300

    P-0213

---
+++ LeetCode/Python/0213_House_Robber_II.py
@@ -0,0 +1,16 @@
+# 213. House Robber II
+# https://leetcode.com/problems/house-robber-ii/
+
+
+class Solution:
+    def rob(self, nums: list[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        a1 = 0
+        b1 = nums[0]
+        a2 = 0
+        b2 = nums[1]
+        for i in range(1, len(nums) - 1):
+            a1, b1 = b1, max(a1 + nums[i], b1)
+            a2, b2 = b2, max(a2 + nums[i + 1], b2)
+        return max(b1, b2)
