18a10efbc1390174d11f620ba7be8aa2890de366
Eren İşci <isci.eren@gmail.com>
2024-07-04 00:05:32 +0300

    P-0989

--- LeetCode/Python/0989_Add_to_Array_Form_of_Integer.py
+++ LeetCode/Python/0989_Add_to_Array_Form_of_Integer.py
@@ -1,5 +1,5 @@
 # 989. Add to Array-Form of Integer
-# https://leetcode.com/problems/add-to-array-form-of-integer/description/
+# https://leetcode.com/problems/add-to-array-form-of-integer/
 
 
 class Solution:
