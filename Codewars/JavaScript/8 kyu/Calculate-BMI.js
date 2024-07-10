e7661b2970bf8c4bcd44a12388b71b6815e6f7dc
Eren İşci <isci.eren@gmail.com>
2024-07-11 00:34:00 +0300

    Calculate BMI

---
+++ Codewars/JavaScript/8 kyu/Calculate-BMI.js
@@ -0,0 +1,7 @@
+function bmi(weight, height) {
+  const bmi = weight / height ** 2;
+  if (bmi <= 18.5) return 'Underweight';
+  else if (bmi <= 25.0) return 'Normal';
+  else if (bmi <= 30.0) return 'Overweight';
+  else return 'Obese';
+}
