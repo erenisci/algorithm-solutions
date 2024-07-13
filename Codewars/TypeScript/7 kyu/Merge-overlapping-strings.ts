0003aeb2f9f3b09c0e49c283f6ba807eb1f1c439
Eren İşci <isci.eren@gmail.com>
2024-07-13 03:14:33 +0300

    Merge overlapping strings

---
+++ Codewars/TypeScript/7 kyu/Merge-overlapping-strings.ts
@@ -0,0 +1,9 @@
+export const mergeStrings = (first: string, second: string): string => {
+  let overlap = 0;
+  for (let i = 0; i < Math.min(first.length, second.length); i++) {
+    if (first.endsWith(second.substring(0, i + 1))) {
+      overlap = i + 1;
+    }
+  }
+  return first + second.substring(overlap);
+};
