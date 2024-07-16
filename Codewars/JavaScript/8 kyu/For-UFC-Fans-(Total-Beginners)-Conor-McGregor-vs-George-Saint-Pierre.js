76d43986d1c3be6daa089d07c688fada94218897
Eren İşci <isci.eren@gmail.com>
2024-07-17 00:05:32 +0300

    For UFC Fans (Total Beginners) Conor McGregor vs George Saint Pierre

---
+++ Codewars/JavaScript/8 kyu/For-UFC-Fans-(Total-Beginners)-Conor-McGregor-vs-George-Saint-Pierre.js
@@ -0,0 +1,5 @@
+function quote(fighter) {
+  return fighter.toLowerCase().startsWith('g')
+    ? 'I am not impressed by your performance.'
+    : "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+}
