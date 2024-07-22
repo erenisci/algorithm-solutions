d71c08006452aae201d24c7dd287f5c2225fa320
Eren İşci <isci.eren@gmail.com>
2024-07-23 01:52:49 +0300

    Leonardo Dicaprio and Oscars

---
+++ Codewars/JavaScript/8 kyu/Leonardo-Dicaprio-and-Oscars.js
@@ -0,0 +1,9 @@
+const leo = oscar => {
+  return oscar === 88
+    ? 'Leo finally won the oscar! Leo is happy'
+    : oscar === 86
+    ? 'Not even for Wolf of wallstreet?!'
+    : oscar < 88
+    ? 'When will you give Leo an Oscar?'
+    : 'Leo got one already!';
+};
