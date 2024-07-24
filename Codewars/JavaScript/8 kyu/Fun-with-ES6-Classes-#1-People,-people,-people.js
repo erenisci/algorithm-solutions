c99276922ac6f6a6c41db959ec1cb7f6dade92ab
Eren İşci <isci.eren@gmail.com>
2024-07-24 04:01:05 +0300

    Fun with ES6 Classes #1 - People, people, people

---
+++ Codewars/JavaScript/8 kyu/Fun-with-ES6-Classes-#1-People,-people,-people.js
@@ -0,0 +1,16 @@
+class Person {
+  constructor(firstName = 'John', lastName = 'Doe', age = 0, gender = 'Male') {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.gender = gender;
+  }
+
+  sayFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  static greetExtraTerrestrials(raceName) {
+    return `Welcome to Planet Earth ${raceName}`;
+  }
+}
