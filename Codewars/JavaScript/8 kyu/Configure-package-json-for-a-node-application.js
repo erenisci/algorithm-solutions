c7c9624da8e47c12fa3e5abbe09696b050b68831
Eren İşci <isci.eren@gmail.com>
2024-07-26 02:25:04 +0300

    Configure package json for a node application

---
+++ Codewars/JavaScript/8 kyu/Configure-package-json-for-a-node-application.js
@@ -0,0 +1,14 @@
+const configuration = {
+  'name': 'my-project',
+  'version': '1.0.0',
+  'description': '',
+  'main': 'index.js',
+  'scripts': {
+    'test': 'echo "Error: no test specified" && exit 1',
+  },
+  'keywords': ['node', 'project'],
+  'author': '',
+  'license': '',
+  'dependencies': {},
+  'devDependencies': {},
+};
