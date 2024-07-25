08bc74dc9188bf00b1f3d6ba19c29416782e3707
Eren İşci <isci.eren@gmail.com>
2024-07-26 02:26:11 +0300

    Grasshopper - Create the rooms

---
+++ Codewars/JavaScript/8 kyu/Grasshopper-Create-the-rooms.js
@@ -0,0 +1,17 @@
+const rooms = {
+  room1: {
+    name: '',
+    description: '',
+    completed: false,
+  },
+  room2: {
+    name: '',
+    description: '',
+    completed: false,
+  },
+  room3: {
+    name: '',
+    description: '',
+    completed: false,
+  },
+};
