b1bb5c129749db5deee0a3fd6d991ddb93495b0f
Eren İşci <isci.eren@gmail.com>
2024-08-02 01:06:29 +0300

    A bugs trilogy Episode 1 - Let Math.Random(); decide your future

---
+++ Codewars/JavaScript/8 kyu/A-bugs-trilogy-Episode-1-Let-Math.Random();-decide-your-future.js
@@ -0,0 +1,7 @@
+function yourFutureCareer() {
+  const career = Math.random();
+
+  if (career <= 0.32) return 'FrontEnd Developer';
+  else if (career <= 0.65) return 'BackEnd Developer';
+  else return 'Full-Stack Developer';
+}
