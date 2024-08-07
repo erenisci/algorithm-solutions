bd5e9d875d39620868b3564077fd2fe89f525b20
Eren İşci <isci.eren@gmail.com>
2024-08-07 21:55:08 +0300

    simple calculator

---
+++ Codewars/JavaScript/8 kyu/simple-calculator.js
@@ -0,0 +1,14 @@
+function calculator(a, b, sign) {
+  switch (sign) {
+    case '+':
+      return a + b;
+    case '-':
+      return a - b;
+    case '*':
+      return a * b;
+    case '/':
+      return a / b;
+    default:
+      return 'unknown value';
+  }
+}
