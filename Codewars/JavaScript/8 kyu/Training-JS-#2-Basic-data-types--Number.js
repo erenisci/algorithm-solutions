d2610b18cae87bb950a2f5d8042e5630c351e3a8
Eren İşci <isci.eren@gmail.com>
2024-08-12 04:09:39 +0300

    Training JS #2 Basic data types--Number

---
+++ Codewars/JavaScript/8 kyu/Training-JS-#2-Basic-data-types--Number.js
@@ -0,0 +1,36 @@
+let v1 = 50,
+  v2 = 100,
+  v3 = 150,
+  v4 = 200,
+  v5 = 2,
+  v6 = 250;
+
+function equal1() {
+  let a = v1,
+    b = v1;
+  return a + b;
+}
+
+function equal2() {
+  let a = v3,
+    b = v1;
+  return a - b;
+}
+
+function equal3() {
+  let a = v1,
+    b = v5;
+  return a * b;
+}
+
+function equal4() {
+  let a = v4,
+    b = v5;
+  return a / b;
+}
+
+function equal5() {
+  let a = v2,
+    b = v3;
+  return a % b;
+}
