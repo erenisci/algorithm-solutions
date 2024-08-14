27ef4aa4ff94b6c2c4d2473c78f818e40f1a79eb
Eren İşci <isci.eren@gmail.com>
2024-08-14 10:53:20 +0300

    Training JS #4 Basic data types--Array

---
+++ Codewars/JavaScript/8 kyu/Training-JS-#4-Basic-data-types--Array.js
@@ -0,0 +1,17 @@
+function getLength(arr) {
+  return arr.length;
+}
+function getFirst(arr) {
+  return arr[0];
+}
+function getLast(arr) {
+  return arr[arr.length - 1];
+}
+function pushElement(arr) {
+  var el = 1;
+  arr.push(el);
+  return arr;
+}
+function popElement(arr) {
+  return arr.splice(0, arr.length - 1);
+}
