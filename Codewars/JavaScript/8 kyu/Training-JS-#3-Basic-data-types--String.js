4587eaf1e11c20bfdb0d33ca93036d3f71feb3de
Eren İşci <isci.eren@gmail.com>
2024-08-14 10:53:06 +0300

    Training JS #3 Basic data types--String

---
+++ Codewars/JavaScript/8 kyu/Training-JS-#3-Basic-data-types--String.js
@@ -0,0 +1,32 @@
+var a1 = 'A',
+  a2 = 'a',
+  b1 = 'B',
+  b2 = 'b',
+  c1 = 'C',
+  c2 = 'c',
+  d1 = 'D',
+  d2 = 'd',
+  e1 = 'E',
+  e2 = 'e',
+  n1 = 'N',
+  n2 = 'n';
+
+function Dad() {
+  return d1 + a2 + d2;
+}
+function Bee() {
+  return b1 + e2 + e2;
+}
+function banana() {
+  return b2 + a2 + n2 + a2 + n2 + a2;
+}
+
+function answer1() {
+  return 'no';
+}
+function answer2() {
+  return 'no';
+}
+function answer3() {
+  return 'yes';
+}
