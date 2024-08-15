1c01d395236f4b1e9a0f487400373cac83b82af1
Eren İşci <isci.eren@gmail.com>
2024-08-16 00:43:44 +0300

    Welcome!

---
+++ Codewars/JavaScript/8 kyu/Welcome!.js
@@ -0,0 +1,23 @@
+function greet(language) {
+  const DB = {
+    english: 'Welcome',
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso',
+  };
+
+  return DB[language] || DB['english'];
+}
