0a657c58caeb891c019a1c4b3808b906b0a51352
Eren İşci <isci.eren@gmail.com>
2024-08-17 18:14:59 +0300

    Training JS #10 loop statement--for

---
+++ Codewars/JavaScript/8 kyu/Training-JS-#10-loop-statement--for.js
@@ -0,0 +1,8 @@
+function pickIt(arr) {
+  const odd = [],
+    even = [];
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] % 2 === 0 ? even.push(arr[i]) : odd.push(arr[i]);
+  }
+  return [odd, even];
+}
