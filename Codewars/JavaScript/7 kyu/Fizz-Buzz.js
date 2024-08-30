9077efc3a475b408a7e9847122a0ae7016cf55f0
Eren İşci <isci.eren@gmail.com>
2024-08-30 06:23:40 +0300

    Fizz Buzz

---
+++ Codewars/JavaScript/7 kyu/Fizz-Buzz.js
@@ -0,0 +1,17 @@
+function fizzbuzz(n) {
+  let result = [];
+
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      result.push('FizzBuzz');
+    } else if (i % 3 === 0) {
+      result.push('Fizz');
+    } else if (i % 5 === 0) {
+      result.push('Buzz');
+    } else {
+      result.push(i);
+    }
+  }
+
+  return result;
+}
