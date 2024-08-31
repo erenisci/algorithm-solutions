1f86d9bf73143a550e91528ca69f6dfd0f3488a0
Eren İşci <isci.eren@gmail.com>
2024-08-31 07:08:14 +0300

    Head, Tail, Init and Last

---
+++ Codewars/JavaScript/7 kyu/Head,-Tail,-Init-and-Last.js
@@ -0,0 +1,15 @@
+function head(arr) {
+  return arr[0];
+}
+
+function tail(arr) {
+  return arr.slice(1);
+}
+
+function init(arr) {
+  return arr.slice(0, -1);
+}
+
+function last(arr) {
+  return arr[arr.length - 1];
+}
