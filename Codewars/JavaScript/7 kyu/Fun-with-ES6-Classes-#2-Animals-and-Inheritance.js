2ee97820d08379cdff91931c4670ada5b587a5be
Eren İşci <isci.eren@gmail.com>
2024-09-03 18:38:30 +0300

    Fun with ES6 Classes #2 - Animals and Inheritance

---
+++ Codewars/JavaScript/7 kyu/Fun-with-ES6-Classes-#2-Animals-and-Inheritance.js
@@ -0,0 +1,26 @@
+class Shark extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 0, 'shark', status);
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 4, 'cat', status);
+  }
+
+  introduce() {
+    return `${super.introduce()}  Meow meow!`;
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, age, status, master) {
+    super(name, age, 4, 'dog', status);
+    this.master = master;
+  }
+
+  greetMaster() {
+    return `Hello ${this.master}`;
+  }
+}
