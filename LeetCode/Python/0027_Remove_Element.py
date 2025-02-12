feab8110624af6d737419f40785c979a054c2e15
Eren İşci <isci.eren@gmail.com>
2025-02-12 07:57:19 +0300

    P-0027

---
+++ LeetCode/Python/0027_Remove_Element.py
@@ -0,0 +1,12 @@
+# 27. Remove Element
+# https://leetcode.com/problems/remove-element/description/
+
+
+class Solution:
+    def removeElement(self, nums: list[int], val: int) -> int:
+        k = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        return k
