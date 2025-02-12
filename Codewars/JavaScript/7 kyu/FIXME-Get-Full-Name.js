c4575efd00268f62420bd6a55a1d6ab598ddf57d
Eren İşci <isci.eren@gmail.com>
2025-02-12 08:15:03 +0300

    FIXME Get Full Name

---
+++ Codewars/JavaScript/7 kyu/FIXME-Get-Full-Name.js
@@ -0,0 +1,10 @@
+class Dinglemouse {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  getFullName() {
+    return `${this.firstName} ${this.lastName}`.trim();
+  }
+}
