54b7e2f1abcb5583be3b1202bb28c8f2c9b115ef
Eren İşci <isci.eren@gmail.com>
2025-07-16 20:36:20 +0300

    Split The Bill

---
+++ Codewars/JavaScript/7 kyu/Split-The-Bill.js
@@ -0,0 +1,16 @@
+function splitTheBill(x) {
+  let average = 0;
+  let count = 0;
+
+  for (item in x) {
+    average += x[item];
+    count++;
+  }
+  average = average / count;
+
+  for (item in x) {
+    x[item] = Math.round((x[item] - average) * 100) / 100;
+  }
+
+  return x;
+}
