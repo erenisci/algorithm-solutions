437713be4c254436ecd2925a4a081a5e5ec928a7
Eren İşci <isci.eren@gmail.com>
2025-07-16 20:36:43 +0300

    ToLeetSpeak

---
+++ Codewars/JavaScript/7 kyu/ToLeetSpeak.js
@@ -0,0 +1,35 @@
+function toLeetSpeak(str) {
+  const letters = {
+    A: '@',
+    B: '8',
+    C: '(',
+    D: 'D',
+    E: '3',
+    F: 'F',
+    G: '6',
+    H: '#',
+    I: '!',
+    J: 'J',
+    K: 'K',
+    L: '1',
+    M: 'M',
+    N: 'N',
+    O: '0',
+    P: 'P',
+    Q: 'Q',
+    R: 'R',
+    S: '$',
+    T: '7',
+    U: 'U',
+    V: 'V',
+    W: 'W',
+    X: 'X',
+    Y: 'Y',
+    Z: '2',
+  };
+
+  return str
+    .split('')
+    .map(char => letters[char] ?? ' ')
+    .join('');
+}
