3cf358e90d239a0925fb85153256e4ee6937d661
Eren İşci <isci.eren@gmail.com>
2025-12-02 20:56:06 +0300

    Letterbox Paint Squad

---
+++ Codewars/JavaScript/7 kyu/Letterbox-Paint-Squad.js
@@ -0,0 +1,11 @@
+var paintLetterboxes = function (start, end) {
+  const counts = Array(10).fill(0);
+
+  for (let n = start; n <= end; n++) {
+    for (const ch of n.toString()) {
+      counts[ch]++;
+    }
+  }
+
+  return counts;
+};
