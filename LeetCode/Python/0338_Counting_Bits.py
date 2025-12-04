9c14037942ea555192758795866526cb7eec9369
Eren İşci <isci.eren@gmail.com>
2025-12-04 04:43:58 +0300

    P-0338

---
+++ LeetCode/Python/0338_Counting_Bits.py
@@ -0,0 +1,11 @@
+# 338. Counting Bits
+# https://leetcode.com/problems/counting-bits/
+
+class Solution:
+    def countBits(self, n: int) -> list[int]:
+        ans = [0] * (n + 1)
+
+        for i in range(1, n + 1):
+            ans[i] = ans[i >> 1] + (i & 1)
+
+        return ans
