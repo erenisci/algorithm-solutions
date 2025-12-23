c2c3f9e11c59ee436d6eeb4307cf9c763ffa5b7e
Eren İşci <isci.eren@gmail.com>
2025-12-23 16:20:43 +0300

    P-0136

---
+++ LeetCode/JavaScript/0136-Single-Number.js
@@ -0,0 +1,15 @@
+// 136. Single Number
+// https://leetcode.com/problems/single-number/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+var singleNumber = function (nums) {
+  let index = 0;
+  for (let i = 0; i < nums.length; i++) {
+    index ^= nums[i];
+  }
+  return index;
+};
