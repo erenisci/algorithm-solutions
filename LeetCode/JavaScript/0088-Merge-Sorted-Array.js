53eba95184353b293b51c491a7ed1bceff40e467
Eren İşci <isci.eren@gmail.com>
2025-12-27 10:00:03 +0300

    P-0088

---
+++ LeetCode/JavaScript/0088-Merge-Sorted-Array.js
@@ -0,0 +1,27 @@
+// 88. Merge Sorted Array
+// https://leetcode.com/problems/merge-sorted-array/
+
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+
+var merge = function (nums1, m, nums2, n) {
+  let i = m - 1;
+  let j = n - 1;
+  let k = m + n - 1;
+
+  while (j >= 0) {
+    if (i >= 0 && nums1[i] > nums2[j]) {
+      nums1[k] = nums1[i];
+      i--;
+    } else {
+      nums1[k] = nums2[j];
+      j--;
+    }
+    k--;
+  }
+};
