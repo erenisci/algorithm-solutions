4edd7625a9c9f67b1a92545e89ab2cebceb05d7c
Eren İşci <isci.eren@gmail.com>
2026-02-14 22:35:06 +0300

    Debug Basic Calculator

---
+++ Codewars/JavaScript/7 kyu/Debug-Basic-Calculator.js
@@ -0,0 +1,22 @@
+function calculate(a, operator, b) {
+  if (!['+', '-', '*', '/'].includes(operator)) {
+    return null;
+  }
+
+  if (operator === '/' && b === 0) {
+    return null;
+  }
+
+  switch (operator) {
+    case '+':
+      return a + b;
+    case '-':
+      return a - b;
+    case '*':
+      return a * b;
+    case '/':
+      return a / b;
+    default:
+      return null;
+  }
+}
