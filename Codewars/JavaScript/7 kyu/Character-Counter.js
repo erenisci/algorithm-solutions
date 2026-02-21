644852b038783d60de25c1f7e6c7727f1771311d
Eren İşci <isci.eren@gmail.com>
2026-02-21 17:34:30 +0300

    Character Counter

--- Codewars/JavaScript/7 kyu/Character-Counter.js
+++ Codewars/JavaScript/7 kyu/Character-Counter.js
@@ -2,9 +2,7 @@ function validateWord(s) {
   const lower = s.toLowerCase();
 
   const counts = {};
-  for (const ch of lower) {
-    counts[ch] = (counts[ch] || 0) + 1;
-  }
+  for (const ch of lower) counts[ch] = (counts[ch] || 0) + 1;
 
   const values = Object.values(counts);
   return values.every(v => v === values[0]);
