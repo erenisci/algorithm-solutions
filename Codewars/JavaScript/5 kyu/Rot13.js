791e49915a916488c5a56b207fa902ce80ac856d
Eren İşci <isci.eren@gmail.com>
2026-02-27 05:20:49 +0300

    Rot13

--- Codewars/JavaScript/5 kyu/Rot13.js
+++ Codewars/JavaScript/5 kyu/Rot13.js
@@ -1,7 +1,9 @@
 function rot13(message) {
   const arr = [...message].map(el => {
-    if (65 <= el.charCodeAt() && el.charCodeAt() <= 90) return el.charCodeAt() + 13 > 90 ? el.charCodeAt() - 13 : el.charCodeAt() + 13;
-    else if (97 <= el.charCodeAt() && el.charCodeAt() <= 122) return el.charCodeAt() + 13 > 122 ? el.charCodeAt() - 13 : el.charCodeAt() + 13;
+    if (65 <= el.charCodeAt() && el.charCodeAt() <= 90)
+      return el.charCodeAt() + 13 > 90 ? el.charCodeAt() - 13 : el.charCodeAt() + 13;
+    else if (97 <= el.charCodeAt() && el.charCodeAt() <= 122)
+      return el.charCodeAt() + 13 > 122 ? el.charCodeAt() - 13 : el.charCodeAt() + 13;
     else return el.charCodeAt();
   });
 
